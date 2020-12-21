b84b0592f62c5e2f4999480f7c228438ce9c1b81
chrl0t <charlotte_i@live.co.uk>
2020-12-21 11:56:14 +0000

    added very basic css to DataCards

--- src/components/DataCards.js
+++ src/components/DataCards.js
@@ -6,13 +6,30 @@ function DataCards({ data: { confirmed, recovered, deaths, lastUpdate } }) {
     return "Loading...";
   }
   return (
-    <div>
-      <h1>Confirmed Cases: {confirmed.value}</h1>
-      <h1>Recovered: {recovered.value}</h1>
-      <h1>Deaths: {deaths.value}</h1>
-      <h1>
-        Last Updated: {format(new Date(lastUpdate), "dd/MM/yyyy' - 'HH:mm")}
-      </h1>
+    <div className='data-cards-container'>
+      <div className='data-card'>
+        <header className='data-card-header'>Infected</header>
+        <div className='data-card-data'>
+          {" "}
+          {confirmed.value}
+          {format(new Date(lastUpdate), "dd/MM/yyyy' - 'HH:mm")}
+        </div>
+      </div>
+      <div className='data-card'>
+        <header className='data-card-header'>Recovered</header>
+        <div className='data-card-data'> {recovered.value}</div>
+      </div>
+      <div className='data-card'>
+        <header className='data-card-header'>Deaths</header>
+        <div className='data-card-data'> {deaths.value}</div>
+      </div>
+      {/* <div className='data-card'>
+        <header className='data-card-header'>Last Updated </header>
+        <div className='data-card-data'>
+          {" "}
+          {format(new Date(lastUpdate), "dd/MM/yyyy' - 'HH:mm")}
+        </div>
+      </div> */}
     </div>
   );
 }
