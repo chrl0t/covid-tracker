1e1545f07d32c3517bc01d913d0c90374a8bf26b
chrl0t <charlotte_i@live.co.uk>
2020-12-21 11:31:56 +0000

    displays data in DataCards

--- src/components/DataCards.js
+++ src/components/DataCards.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-function DataCards() {
+function DataCards({ data: { confirmed, recovered, deaths, lastUpdate } }) {
+  if (!confirmed) {
+    return "Loading...";
+  }
   return (
     <div>
-      <h1>DataCards</h1>
+      <h1>Confirmed Cases: {confirmed.value}</h1>
+      <h1>Recovered: {recovered.value}</h1>
+      <h1>Deaths: {deaths.value}</h1>
+      <h1>Last Updated: {lastUpdate}</h1>
     </div>
   );
 }
