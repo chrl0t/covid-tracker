188d05f9f747ec8d30de88ef810f88fef7c2f300
chrl0t <charlotte_i@live.co.uk>
2020-12-21 10:57:42 +0000

    added chart component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import DataCards from "./components/DataCards";
 import CountriesList from "./components/CountriesList";
+import Chart from "./components/Chart";
 
 function App() {
   return (
     <div className='container'>
       <DataCards />
       <CountriesList />
+      <Chart />
     </div>
   );
 }
---
+++ src/components/Chart.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Chart extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Chart</h1>
+      </div>
+    );
+  }
+}
+
+export default Chart;
