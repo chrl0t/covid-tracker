ec22859dd83435a5197a383104ccb4c18f3967ae
chrl0t <charlotte_i@live.co.uk>
2020-12-21 10:55:58 +0000

    added basic styling and created countries list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import DataCards from "./components/DataCards";
+import CountriesList from "./components/CountriesList";
 
 function App() {
   return (
-    <div>
+    <div className='container'>
       <DataCards />
+      <CountriesList />
     </div>
   );
 }
---
+++ src/components/CountriesList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class CountriesList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Countries List</h1>
+      </div>
+    );
+  }
+}
+
+export default CountriesList;
