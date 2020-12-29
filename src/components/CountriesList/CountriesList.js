63437fa218532cd604f79341d18954c7dcf0ccc6
chrl0t <charlotte_i@live.co.uk>
2020-12-29 11:05:52 +0000

    inputted data into country picker

--- src/components/CountriesList/CountriesList.js
+++ src/components/CountriesList/CountriesList.js
@@ -19,6 +19,11 @@ function CountriesList() {
     <FormControl className={styles.FormControl}>
       <NativeSelect>
         <option value='global'>Global</option>
+        {fetchedCountries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
       </NativeSelect>
     </FormControl>
   );
